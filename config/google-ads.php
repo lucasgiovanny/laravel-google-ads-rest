1de7130219a142de35a7551a00f3eb38bdceecc8
Lucas Giovanny <lucasgiovanny@gmail.com>
2021-08-09 17:54:21 +0100

    Work in progress

--- config/google-ads.php
+++ config/google-ads.php
@@ -15,5 +15,10 @@ return [
 
     'client-secret' => env('GOOGLEADS_CLIENT_SECRET'),
 
+    'developer-token' => env('GOOGLEADS_DEVELOPER_TOKEN'),
+
     'refresh-token' => env('GOOGLEADS_REFRESH_TOKEN'),
+
+    'default-account' => env('GOOGLEADS_DEFAULT_ACCOUNT'),
+
 ];
